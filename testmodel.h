3505b505e560fc3bd13ed366171c68d3cc7e7ec5
Robelashame <rasha679@mtroyal.ca>
2026-03-01 13:31:35 -0700

    Started testmodel

---
+++ testmodel.h
@@ -0,0 +1,24 @@
+#ifndef TESTMODEL_H
+#define TESTMODEL_H
+
+#include <stdbool.h>
+
+#include "model.h"
+
+void test_two_player(Model *game);
+
+void test_game_start(Model *game);
+
+void test_hit(Model *game);
+
+void test_stand(Model *game);
+
+void test_win(Model *game);
+
+void test_lose(Model *game);
+
+void test_new_round(Model *game);
+
+void test_quit(Model *game);
+
+#endif
