e0c99f96283ad1dee72c67f6ad52021e1c87c914
Robelashame <rasha679@mtroyal.ca>
2026-03-02 23:26:21 -0700

    did one small fix

--- gameobjects/dealer.c
+++ gameobjects/dealer.c
@@ -6,10 +6,9 @@ void show_hidden(Dealer *dealer) {
 }
 
 void dealer_play(Dealer *dealer, Deck *shoe) {
-    
-    add_card(&dealer->hand, &dealer->hidden_card);
-
     Card dealt_card;
+
+    add_card(&dealer->hand, &dealer->hidden_card);
 
     while (dealer->hand.value < 17){
         dealt_card = deal(shoe);
