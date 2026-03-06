42b49f545abb3bd03d636d6d572b064a00b853a9
JustDanu <danielrusu8100@gmail.com>
2026-03-06 01:55:51 -0700

    Added clubs and started on the card drawer func

--- TST_RAST.C
+++ TST_RAST.C
@@ -25,8 +25,14 @@ const UINT16 heart[] = {
 	0xffff, 0xffff, 0x7ffe, 0x3ffc, 0x1ff8, 0x0ff0, 0x07e0, 0x03c0
 };
 
+/* 16x16 Club shape */
+const UINT16 club[] = {
+    0x03c0, 0x07e0, 0x0ff0, 0x0ff0, 0x0ff0, 0x07e0, 0x3bdc, 0x7ffe, 
+	0xffff, 0xffff, 0xffff, 0x7dbe, 0x399c, 0x03c0, 0x07e0, 0x1ff8
+};
 
-/* 32x10 Downward Arrow */
+
+/* 32x10 Left Arrow */
 const UINT32 bitmap32[] = {
     0x00000F00, 0x0000FF00, 0x000FFFFF, 0x00FFFFFF, 0x0FFFFFFF,
     0x0FFFFFFF, 0x00FFFFFF, 0x000FFFFF, 0x0000FF00, 0x00000F00
@@ -46,11 +52,8 @@ int main()
 
 	plot_8bit_bitmap(base, 20, 20, bitmap8, 8);
 	plot_16bit_bitmap((UINT16 *)base, 20, 50, spade, 16);
-	plot_16bit_bitmap((UINT16 *)base, 110, 54, heart, 16);
-	plot_character(base, 102, 59, 'A');
-	plot_character(base, 1020, 610, 'A');
-	plot_rectangle(base, 100, 50, 200, 100);
 	plot_16bit_bitmap((UINT16 *)base, 20, 80, diamond, 10);
+	plot_16bit_bitmap((UINT16 *)base, 80, 100, club, 16);
 	plot_32bit_bitmap((UINT32 *)base, 50, 100, bitmap32, 10);
 	
 	/*
@@ -79,4 +82,12 @@ int main()
 	*/
 
 	return 0;
+}
+
+void(UINT8 *base, int row, int col, UINT16[] suit, char num)
+{
+	/*Work in progress card drawer, didnt feel like doin math n shi so i left it like this*/
+	plot_16bit_bitmap((UINT16 *)base, 110, 54, heart, 16);
+	plot_character(base, 102, 59, 'A');
+	plot_rectangle(base, 100, 50, 200, 100);
 }
