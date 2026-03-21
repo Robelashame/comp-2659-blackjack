5fd223dc59f1065852d43b24e35f60c50c7ab0bd
Robelashame <rasha679@mtroyal.ca>
2026-03-20 22:03:32 -0600

    added wait time and changed getime to time

--- getime.h
+++
@@ -1,7 +0,0 @@
-#ifndef GETIME_H
-#define GETIME_H
-#include "TYPES.H"
-
-UINT32 get_time();
-
-#endif
--- time.c
+++ time.c
@@ -1,4 +1,4 @@
-#include "getime.h"
+#include "time.h"
 
 UINT32 get_time() {
     long *timer;
@@ -10,4 +10,10 @@ UINT32 get_time() {
     Super(old_ssp);       /* exit privileged mode */
 
     return time;
+}
+
+void wait_for_vbl(UINT32 old_time) {
+    while (get_time() == old_time) {
+        /*waiting*/
+    }
 }
