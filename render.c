f52b1dfec7b347b9e4611bec21588ffdd82954ef
Sidney Nguyen <sydneynguyen05@yahoo.ca>
2026-04-02 01:09:50 -0600

    split up render timer, still working on render player

--- render.c
+++ render.c
@@ -2,6 +2,9 @@
 #include "string.h"
 #include "stdio.h"
 
+static RenderSnapshot front_snapshot;
+static RenderSnapshot back_snapshot;
+
 void render_card(const Card *card, UINT8 *base) {
     char rank[10];
     char suit[10];
@@ -66,6 +69,29 @@ void render_player(const Player *player, UINT8 *base) {
     render_hand(&player->hand, base);
 }
 
+void render_player_text(const Player *player, UINT8 *base) {
+    plot_string(base, player->hand_position[0] + 105, player->hand_position[1], bank);
+    plot_string(base, player->hand_position[0] - 20, player->hand_position[1], value);
+    plot_string(base, player->hand_position[0] - 40, player->hand_position[1], bet);
+}
+
+void render_player_cards(const Player *player, UINT8 *base) {
+    char bank_int[10];
+    char value_int[10];
+    char bet_int[10];
+
+    /* convert integers to strings */
+    sprintf(bank_int, "%d", player->bank);
+    sprintf(value_int, "%d", player->hand.value);
+    sprintf(bet_int, "%d", player->total_bet);
+
+    plot_string(base, player->hand_position[0] + 105, player->hand_position[1], bank);
+    plot_string(base, player->hand_position[0] - 20, player->hand_position[1], value);
+    plot_string(base, player->hand_position[0] - 40, player->hand_position[1], bet);
+
+
+}
+
 void render_dealer(const Dealer *dealer, UINT8 *base) {
     char value[20] = "Value: ";
     char value_int[10];
@@ -79,17 +105,18 @@ void render_dealer(const Dealer *dealer, UINT8 *base) {
     plot_string(base, dealer->position[0] - 20, dealer->position[1], value);
 }
 
-void render_timer(const Timer *timer, UINT8 *base) {
-    char timer_str[20] = "Seconds Left: ";
+void render_timer_text(const Timer *timer, UINT8 *base) {
+    plot_string(base, 200, 280, "Seconds Left: ");
+}
+
+void render_timer_tick(const Timer *timer, UINT8 *base) {
     char seconds_int[10];
 
     /* convert integer seconds to string */
     sprintf(seconds_int, "%d", timer->seconds);
 
-    /* append the number to the string */
-    strcat(timer_str, seconds_int);
-
-    plot_string(base, 200, 280, timer_str);
+    clear_region(base, 200, 330, 16, 8);
+    plot_string(base, 200, 330, seconds_int);
 }
 
 void render(const Model *model, UINT8 *base) {
@@ -98,4 +125,29 @@ void render(const Model *model, UINT8 *base) {
     render_timer(&model->timer, base);
     if (model->is_there_player2)
         render_player(&model->player2, base);
+}
+
+void create_snapshot(const Model *game, RenderSnapshot *snap) {
+    snap->player1 = game->player1;
+    snap->player2 = game->player2;
+    snap->dealer = game->dealer;
+    snap->timer = game->timer;
+
+    snap->is_there_player2 = game->is_there_player2;
+
+    snap->player1_bet = game->player1_bet;
+    snap->player2_bet = game->player2_bet;
+
+    snap->player1_turn = game->player1_turn;
+    snap->player2_turn = game->player2_turn;
+    snap->dealer_turn = game->dealer_turn;
+
+    snap->dealing_start_cards = game->dealing_start_cards;
+    snap->outcome_applied = game->outcome_applied;
+    snap->is_round_over = game->is_round_over;
+    snap->is_game_over = game->is_game_over;
+}
+
+void render_min(const Model *model, UINT8 *base) {
+    
 }
--- render.h
+++ render.h
@@ -6,9 +6,34 @@
 #include "model.h"
 #include "bmaps.h"
 
+typedef struct {
+    Player player1;
+    Player player2;
+    Dealer dealer;
+    Timer timer;
+
+    int is_there_player2;
+
+    int player1_bet;
+    int player2_bet;
+
+    int player1_turn;   
+    int player2_turn;
+    int dealer_turn;
+
+    int dealing_start_cards;
+    int outcome_applied;
+    int is_round_over;
+    int is_game_over;
+} RenderSnapshot;
+
 void render(const Model *model, UINT8 *base);
 
 void render_player(const Player *player, UINT8 *base);
+
+void render_player_text(const Player *player, UINT8 *base);
+
+void render_player_cards(const Player *player, UINT8 *base);
 
 void render_hand(const Hand *hand, UINT8 *base);
 
@@ -16,6 +41,14 @@ void render_card(const Card *card, UINT8 *base);
 
 void render_dealer(const Dealer *dealer, UINT8 *base);
 
-void render_timer(const Timer *timer, UINT8 *base);
+void render_dealer_text(const Dealer *dealer, UINT8 *base);
+
+void render_dealer_cards(const Dealer *dealer, UINT8 *base);
+
+void render_timer_text(const Timer *timer, UINT8 *base);
+
+void render_timer_tick(const Timer *timer, UINT8 *base);
+
+void create_snapshot(const Model *game, RenderSnapshot *snapshot);
 
 #endif
